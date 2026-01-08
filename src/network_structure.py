e096b882f24e318c1de218c3cf86ea083453800b
yohanajocelyn <yohanajoycelyn@gmail.com>
2026-01-08 21:58:23 +0700

    Refactor model training to support balanced datasets
    Updated network_structure.py to allow for dataset balancing using RandomUnderSampler from imblearn, though the balancing code is currently commented out. All model training and structure search now use the potentially balanced dataframe, improving flexibility for handling imbalanced data.

--- src/network_structure.py
+++ src/network_structure.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pgmpy.models import DiscreteBayesianNetwork
 from pgmpy.estimators import HillClimbSearch, BayesianEstimator, BIC
 from pgmpy.inference import VariableElimination
+from imblearn.under_sampling import RandomUnderSampler
 
 def load_and_train_model(csv_path):
     # Load the dataset
@@ -12,14 +13,24 @@ def load_and_train_model(csv_path):
     for col in df.columns:
         if col != 'TYPE':
             df[col] = df[col].astype(int)
+    
+    # X = df.drop(columns=['TYPE'])
+    # y = df['TYPE']
+
+    # rus = RandomUnderSampler(random_state=42)
+    # X_resampled, y_resampled = rus.fit_resample(X, y)
+
+    # df_balanced = pd.concat([X_resampled, y_resampled], axis=1)
+
+    df_balanced = df
 
     # Set the HillClimbSearch buat df tersebut
-    hc = HillClimbSearch(df)
+    hc = HillClimbSearch(df_balanced)
     
     # Proses mencari struktur yang terbaik dengan BIC scoring
     # Max iteration dijadikan 2000
     best_structure = hc.estimate(
-        scoring_method=BIC(df), 
+        scoring_method=BIC(df_balanced), 
         max_iter=2000, 
         show_progress=True
     )
@@ -27,11 +38,11 @@ def load_and_train_model(csv_path):
     # Bikin gambar Bayesian Network berdasarkan struktur terbaik yang ditemukan + add nodes dari kolom-kolom datasetnya
     # karena best_structure hanya memberikan relationshipnya saja
     model = DiscreteBayesianNetwork(best_structure.edges())
-    model.add_nodes_from(df.columns)
+    model.add_nodes_from(df_balanced.columns)
     
     # Dari relationship yang sudah ditemukan, fit ke modelnya untuk belajar probabilitynya
     model.fit(
-        df, 
+        df_balanced, 
         estimator=BayesianEstimator, 
         prior_type="BDeu", # 
         equivalent_sample_size=5 # Untuk mencegah probabilitas 0 kalau semisal penyakit dan gejala yang tidak berhubungan
@@ -39,4 +50,4 @@ def load_and_train_model(csv_path):
     
     # Model matematika yang akan menghitungkan probabilitas berdasarkan evidence (gejala yang sudah dijawab) pakai .query()
     infer = VariableElimination(model)
-    return model, inf
+    return model, infer
